13ba335efac42d1b8724054d78470046a2a19b23
teresiawairimu <teresiawairimu06@gmail.com>
2024-11-05 02:08:10 +0000

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map with various nested dictionary structures and
         paths
         Parameters
+        ----------
         nested_map: dict
             The nested dictionary structure to be accessed
         path: tuple
@@ -26,11 +27,34 @@ class TestAccessNestedMap(unittest.TestCase):
         expected: any
             THe expected result from accessing the path in the nested map
         Asserts
+        -------
         assertEqual
             Ensures that access_nested_map(nested_map, path)
             matches the expected value
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test that access_nested_map raises a KeyError for invalid paths.
+        Parameters
+        ----------
+        nested_map : dict
+            The nested dictionary structure to be accessed.
+        path : tuple
+            The sequence of keys representing an invalid path to the desired value.
+        Asserts
+        -------
+        assertRaises
+            Ensures that a KeyError is raised when an invalid path is provided.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 
 if __name__ == "__main__":
